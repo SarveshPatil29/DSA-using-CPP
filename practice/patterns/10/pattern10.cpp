b05c2287810c5de5489ed61df17ca26d826dca87
Sarvesh Patil <sarvesh2902@gmail.com>
2022-10-31 09:36:25 +0530

    added pattern 10

---
+++ practice/patterns/10/pattern10.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << endl;
+
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= i)
+        {
+            char c = 'A' + i - 1;
+            cout << c << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    return 0;
+}
