25a709e64dd41adc183660d5fb67bcee3a4f9eb5
Sarvesh Patil <sarvesh2902@gmail.com>
2022-11-10 08:33:05 +0530

    solved ap ques

---
+++ practice/problems/arithmeticProgression.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    int ap = 3 * n + 7;
+    cout << "nth term is: " << ap << endl;
+
+    return 0;
+}
