e552e03c8a298f3ee986b7a719fdd07f29f13eea
Sarvesh Patil <sarvesh2902@gmail.com>
2022-11-16 09:46:12 +0530

    solved Intersection Of Two Sorted Arrays

---
+++ codeStudio/intersectionOfTwoSortedArrays.cpp
@@ -0,0 +1,29 @@
+// https://www.codingninjas.com/codestudio/problems/intersection-of-2-arrays_1082149
+
+#include <bits/stdc++.h>
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+    vector<int> ans;
+    int i = 0;
+    int j = 0;
+
+    while (i < arr1.size() && j < arr2.size())
+    {
+        if (arr1[i] == arr2[j])
+        {
+            ans.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+        else if (arr1[i] < arr2[j])
+        {
+            i++;
+        }
+        else if (arr1[i] > arr2[j])
+        {
+            j++;
+        }
+    }
+
+    return ans;
+}
