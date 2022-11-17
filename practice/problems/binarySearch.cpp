a34d2f5a4c17adebaae9b34511deaec40be7d8dd
Sarvesh Patil <sarvesh2902@gmail.com>
2022-11-17 09:36:30 +0530

    implemented binary search

---
+++ practice/problems/binarySearch.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = (start + end) / 2;
+
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        else if (arr[mid] > key)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+        mid = (start + end) / 2;
+    }
+    return -1;
+}
+
+int main()
+{
+    int size = 0;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int arr[size];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int key = 5;
+    cout << "Enter the element to search: ";
+    cin >> key;
+
+    cout << "Index of " << key << " is: " << binarySearch(arr, sizeof(arr) / sizeof(arr[0]), key) << endl;
+}
