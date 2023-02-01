278117a4ec35def554f1d033cb5b629591416914
Sarvesh Patil <sarvesh2902@gmail.com>
2023-02-01 19:16:15 +0530

    Excel Sheet Column Title

---
+++ leetcode/0168.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/excel-sheet-column-title/
+
+class Solution
+{
+public:
+  string convertToTitle(int columnNumber)
+  {
+    string ans = "";
+
+    while (columnNumber > 0)
+    {
+      columnNumber--;
+      int a = columnNumber % 26;
+      char b = 'A' + a;
+
+      ans += b;
+      columnNumber /= 26;
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    return ans;
+  }
+};
