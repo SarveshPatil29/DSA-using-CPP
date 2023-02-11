3be8b605f07fe6dafa48d427716ba9051738cb8f
Sarvesh Patil <sarvesh2902@gmail.com>
2023-02-11 11:13:58 +0530

    Maximum Subarray

---
+++ leetcode/0053.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/maximum-subarray/
+// Kadane's Algorithm
+
+class Solution
+{
+public:
+  int maxSubArray(vector<int> &nums)
+  {
+    int maxSum = INT_MIN;
+    int sum = 0;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+      sum += nums[i];
+
+      if (sum > maxSum)
+      {
+        maxSum = sum;
+      }
+
+      if (sum < 0)
+      {
+        sum = 0;
+      }
+    }
+
+    return maxSum;
+  }
+};
