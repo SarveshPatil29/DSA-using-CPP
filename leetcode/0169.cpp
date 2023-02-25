61c37b7dc8a2028a61e384f6d0e89ef3abd8932f
Sarvesh Patil <sarvesh2902@gmail.com>
2023-02-25 10:56:01 +0530

    added optimized solution for problem 169

--- leetcode/0169.cpp
+++ leetcode/0169.cpp
@@ -1,33 +1,63 @@
 // https://leetcode.com/problems/majority-element/
 
+// Moore's Voting Algorithm
 class Solution
 {
 public:
   int majorityElement(vector<int> &nums)
   {
-    unordered_map<int, int> map;
-
-    if (nums.size() == 1)
-    {
-      return nums[0];
-    }
+    int num = -1;
+    int count = 0;
 
     for (int i = 0; i < nums.size(); i++)
     {
-      if (map.find(nums[i]) == map.end())
+      if (num == nums[i])
       {
-        map[nums[i]] = 1;
+        count++;
+      }
+      else if (count == 0)
+      {
+        num = nums[i];
+        count = 1;
       }
       else
       {
-        map[nums[i]]++;
-        if (map[nums[i]] > nums.size() / 2)
-        {
-          return nums[i];
-        }
+        count--;
       }
     }
 
-    return -1;
+    return num;
   }
 };
+
+// class Solution
+// {
+// public:
+//   int majorityElement(vector<int> &nums)
+//   {
+//     unordered_map<int, int> map;
+
+//     if (nums.size() == 1)
+//     {
+//       return nums[0];
+//     }
+
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//       if (map.find(nums[i]) == map.end())
+//       {
+//         map[nums[i]] = 1;
+//       }
+//       else
+//       {
+//         map[nums[i]]++;
+//         if (map[nums[i]] > nums.size() / 2)
+//         {
+//           return nums[i];
+//         }
+//       }
+//     }
+
+//     return -1;
+//   }
+// };
