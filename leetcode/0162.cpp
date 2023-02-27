7b2470a46aefbaf2238e603416d37b7c78b00f2f
Sarvesh Patil <sarvesh2902@gmail.com>
2023-02-27 10:24:40 +0530

    added optimized soln in 162

--- leetcode/0162.cpp
+++ leetcode/0162.cpp
@@ -5,16 +5,40 @@ class Solution
 public:
   int findPeakElement(vector<int> &nums)
   {
-    for (int i = 0; i < nums.size(); i++)
+    int n = nums.size();
+    int low = 0;
+    int high = nums.size() - 1;
+    while (low < high)
     {
-      if (i + 1 == nums.size() || (i + 1 < nums.size() && nums[i] > nums[i + 1]))
+      int mid = (low + high) >> 1;
+      if (nums[mid] > nums[mid + 1])
       {
-        if (i - 1 == -1 || i - 1 > -1 && nums[i] > nums[i - 1])
-        {
-          return i;
-        }
+        high = mid;
+      }
+      else
+      {
+        low = mid + 1;
       }
     }
-    return -1;
+    return low;
   }
 };
+
+// class Solution
+// {
+// public:
+//   int findPeakElement(vector<int> &nums)
+//   {
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//       if (i + 1 == nums.size() || (i + 1 < nums.size() && nums[i] > nums[i + 1]))
+//       {
+//         if (i - 1 == -1 || i - 1 > -1 && nums[i] > nums[i - 1])
+//         {
+//           return i;
+//         }
+//       }
+//     }
+//     return -1;
+//   }
+// };
