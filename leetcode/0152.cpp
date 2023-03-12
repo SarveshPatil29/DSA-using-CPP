085eb67f539404eeee0448da8ed31cee5d9470fb
Sarvesh Patil <sarvesh2902@gmail.com>
2023-03-12 19:16:03 +0530

    Maximum Product Subarray

---
+++ leetcode/0152.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/maximum-product-subarray/
+
+class Solution
+{
+public:
+  int maxProduct(vector<int> &nums)
+  {
+    int ans = nums[0];
+    int ma = nums[0];
+    int mi = nums[0];
+
+    for (int i = 1; i < nums.size(); i++)
+    {
+      if (nums[i] < 0)
+      {
+        swap(ma, mi);
+      }
+      ma = max(nums[i], ma * nums[i]);
+      mi = min(nums[i], mi * nums[i]);
+      ans = max(ans, ma);
+    }
+    return ans;
+  }
+};
