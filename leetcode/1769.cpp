0bb70020642b8832c3bfb9aff68caa299e630573
Sarvesh Patil <sarvesh2902@gmail.com>
2023-03-14 14:36:54 +0530

    Optimized Minimum Number of Operations to Move All Balls to Each Box

--- leetcode/1769.cpp
+++ leetcode/1769.cpp
@@ -7,19 +7,57 @@ public:
   {
     int n = boxes.size();
     vector<int> ans(n);
+    int ahead = 0;
+    int behind = 0;
 
     for (int i = 0; i < n; i++)
     {
-      int count = 0;
-      for (int j = 0; j < n; j++)
+      if (boxes[i] == '1')
       {
-        if (j != i && boxes[j] == '1')
-        {
-          count += abs(j - i);
-        }
+        ans[0] += i;
+        ahead++;
       }
-      ans[i] = count;
+    }
+
+    if (boxes[0] == '1')
+    {
+      ahead--;
+      behind++;
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+      ans[i] = ans[i - 1] - ahead + behind;
+      if (boxes[i] == '1')
+      {
+        ahead--;
+        behind++;
+      }
     }
     return ans;
   }
 };
+
+// class Solution
+// {
+// public:
+//   vector<int> minOperations(string boxes)
+//   {
+//     int n = boxes.size();
+//     vector<int> ans(n);
+
+//     for (int i = 0; i < n; i++)
+//     {
+//       int count = 0;
+//       for (int j = 0; j < n; j++)
+//       {
+//         if (j != i && boxes[j] == '1')
+//         {
+//           count += abs(j - i);
+//         }
+//       }
+//       ans[i] = count;
+//     }
+//     return ans;
+//   }
+// };
