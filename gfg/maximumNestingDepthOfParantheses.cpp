0938d02ebc8e5c0d0179d264c0281718b6095a90
Sarvesh Patil <sarvesh2902@gmail.com>
2023-03-21 09:57:41 +0530

    Maximum Nesting Depth of the Parentheses

---
+++ gfg/maximumNestingDepthOfParantheses.cpp
@@ -0,0 +1,30 @@
+// https://practice.geeksforgeeks.org/problems/maximum-nesting-depth-of-the-parentheses/1
+
+class Solution
+{
+public:
+  int maxDepth(string s)
+  {
+    int ans = 0;
+    vector<char> stack;
+    int temp = 0;
+    int length = s.length();
+
+    for (int i = 0; i < length; i++)
+    {
+      if (s[i] == '(')
+      {
+        stack.push_back('(');
+        temp++;
+      }
+      else if (s[i] == ')')
+      {
+        stack.pop_back();
+        ans = max(ans, temp);
+        temp--;
+      }
+    }
+
+    return ans;
+  }
+};
