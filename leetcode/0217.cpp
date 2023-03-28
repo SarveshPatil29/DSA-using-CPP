200114ce687d0f9f89a453e80941243115aeb592
Sarvesh Patil <sarvesh2902@gmail.com>
2023-03-28 14:44:28 +0530

    Contains Duplicate

---
+++ leetcode/0217.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/contains-duplicate/
+
+class Solution
+{
+public:
+  bool containsDuplicate(vector<int> &nums)
+  {
+    unordered_map<int, int> map;
+    for (int i = 0; i < nums.size(); i++)
+    {
+      if (map.find(nums[i]) != map.end())
+      {
+        return true;
+      }
+      else
+      {
+        map[nums[i]]++;
+      }
+    }
+    return false;
+  }
+};
