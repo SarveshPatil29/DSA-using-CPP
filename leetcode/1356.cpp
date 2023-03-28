07e1774fd5200c6c3f46751fa051e96a204f2a24
Sarvesh Patil <sarvesh2902@gmail.com>
2023-03-28 16:15:23 +0530

    Sort Integers by The Number of 1 Bits

---
+++ leetcode/1356.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/
+
+class Solution
+{
+public:
+  vector<int> sortByBits(vector<int> &arr)
+  {
+    vector<int> ans;
+    vector<pair<int, int>> v;
+    for (auto val : arr)
+    {
+      int dem = val;
+      int cnt = 0;
+      while (dem > 0)
+      {
+        cnt += (dem & 1);
+        dem = dem >> 1;
+      }
+      v.push_back({cnt, val});
+    }
+    sort(v.begin(), v.end());
+    for (auto val : v)
+      ans.push_back(val.second);
+    return ans;
+  }
+};
