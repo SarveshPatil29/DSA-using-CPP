f4a74dd1fc4fe787dd856ecb103376657d5d52bb
Sarvesh Patil <sarvesh2902@gmail.com>
2023-03-29 09:21:19 +0530

    optimized 0003

--- leetcode/0003.cpp
+++ leetcode/0003.cpp
@@ -5,27 +5,60 @@ class Solution
 public:
   int lengthOfLongestSubstring(string s)
   {
+    set<char> mySet;
     int maxLength = 0;
-    for (int i = 0; i < s.length(); i++)
+    int left = 0;
+    for (int right = 0; right < s.length(); right++)
     {
-      int length = 1;
-      set<char> mySet;
-      mySet.insert(s[i]);
-      for (int j = i + 1; j < s.length(); j++)
+      if (!mySet.count(s[right]))
       {
-        if (mySet.count(s[j]))
-        {
-          break;
-        }
-        else
-        {
-          mySet.insert(s[j]);
-          length++;
-        }
+        mySet.insert(s[right]);
+        maxLength = max(maxLength, right - left + 1);
       }
-      maxLength = max(maxLength, length);
+      else
+      {
+        while (s[right] != s[left])
+        {
+          mySet.erase(s[left]);
+          left++;
+        }
+
+        mySet.erase(s[left]);
+        left++;
+        mySet.insert(s[right]);
+      }
     }
 
     return maxLength;
   }
 };
+
+// class Solution
+// {
+// public:
+//   int lengthOfLongestSubstring(string s)
+//   {
+//     int maxLength = 0;
+//     for (int i = 0; i < s.length(); i++)
+//     {
+//       int length = 1;
+//       set<char> mySet;
+//       mySet.insert(s[i]);
+//       for (int j = i + 1; j < s.length(); j++)
+//       {
+//         if (mySet.count(s[j]))
+//         {
+//           break;
+//         }
+//         else
+//         {
+//           mySet.insert(s[j]);
+//           length++;
+//         }
+//       }
+//       maxLength = max(maxLength, length);
+//     }
+
+//     return maxLength;
+//   }
+// };
