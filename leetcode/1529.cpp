79b58fd848903dac7a3cdcc561c515aa66044fba
Sarvesh Patil <sarvesh2902@gmail.com>
2023-03-29 16:54:51 +0530

    optimized 1529

--- leetcode/1529.cpp
+++ leetcode/1529.cpp
@@ -5,30 +5,50 @@ class Solution
 public:
   int minFlips(string target)
   {
-    string s = "";
-    for (int i = 0; i < target.size(); i++)
-    {
-      s.append("0");
-    }
-
+    char status = '0';
     int ans = 0;
 
-    bool isFlipped = false;
-
     for (int i = 0; i < target.size(); i++)
     {
-      if (target[i] != s[i] && isFlipped == false)
+      if (target[i] != status)
       {
-        isFlipped = !isFlipped;
-        ans++;
-      }
-      else if (target[i] == s[i] && isFlipped == true)
-      {
-        isFlipped = !isFlipped;
+        status = status == '0' ? '1' : '0';
         ans++;
       }
     }
-
     return ans;
   }
 };
+
+// class Solution
+// {
+// public:
+//   int minFlips(string target)
+//   {
+//     string s = "";
+//     for (int i = 0; i < target.size(); i++)
+//     {
+//       s.append("0");
+//     }
+
+//     int ans = 0;
+
+//     bool isFlipped = false;
+
+//     for (int i = 0; i < target.size(); i++)
+//     {
+//       if (target[i] != s[i] && isFlipped == false)
+//       {
+//         isFlipped = !isFlipped;
+//         ans++;
+//       }
+//       else if (target[i] == s[i] && isFlipped == true)
+//       {
+//         isFlipped = !isFlipped;
+//         ans++;
+//       }
+//     }
+
+//     return ans;
+//   }
+// };
