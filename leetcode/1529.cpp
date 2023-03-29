c993d208d717aa189ee44e0bbdb992bc046c34f0
Sarvesh Patil <sarvesh2902@gmail.com>
2023-03-29 16:44:13 +0530

    Minimum Suffix Flips

---
+++ leetcode/1529.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/minimum-suffix-flips/
+
+class Solution
+{
+public:
+  int minFlips(string target)
+  {
+    string s = "";
+    for (int i = 0; i < target.size(); i++)
+    {
+      s.append("0");
+    }
+
+    int ans = 0;
+
+    bool isFlipped = false;
+
+    for (int i = 0; i < target.size(); i++)
+    {
+      if (target[i] != s[i] && isFlipped == false)
+      {
+        isFlipped = !isFlipped;
+        ans++;
+      }
+      else if (target[i] == s[i] && isFlipped == true)
+      {
+        isFlipped = !isFlipped;
+        ans++;
+      }
+    }
+
+    return ans;
+  }
+};
