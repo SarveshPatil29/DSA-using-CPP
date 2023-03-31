1eada32c86b3279482836d82a151ad0671d64810
Sarvesh Patil <sarvesh2902@gmail.com>
2023-03-31 18:55:11 +0530

    optimized 977

--- leetcode/0977.cpp
+++ leetcode/0977.cpp
@@ -5,13 +5,67 @@ class Solution
 public:
   vector<int> sortedSquares(vector<int> &nums)
   {
-    for (int i = 0; i < nums.size(); i++)
+    int pos = 0;
+    int neg = 0;
+    int i;
+    for (i = 0; i < nums.size(); i++)
     {
-      nums[i] *= nums[i];
+      if (nums[i] >= 0)
+      {
+        break;
+      }
     }
 
-    sort(nums.begin(), nums.end());
+    pos = i;
+    neg = i - 1;
+    vector<int> ans(nums.size(), 0);
 
-    return nums;
+    i = 0;
+    while (pos < nums.size() && neg >= 0)
+    {
+      if (nums[pos] > (-1 * nums[neg]))
+      {
+        ans[i] = pow(nums[neg], 2);
+        neg--;
+      }
+      else
+      {
+        ans[i] = pow(nums[pos], 2);
+        pos++;
+      }
+      i++;
+    }
+
+    while (pos < nums.size())
+    {
+      ans[i] = pow(nums[pos], 2);
+      pos++;
+      i++;
+    }
+
+    while (neg >= 0)
+    {
+      ans[i] = pow(nums[neg], 2);
+      neg--;
+      i++;
+    }
+
+    return ans;
   }
 };
+
+// class Solution
+// {
+// public:
+//   vector<int> sortedSquares(vector<int> &nums)
+//   {
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//       nums[i] *= nums[i];
+//     }
+
+//     sort(nums.begin(), nums.end());
+
+//     return nums;
+//   }
+// };
