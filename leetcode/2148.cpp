7a1e1ea9aa3f17bdf8225dd4430c952044a1c7e9
Sarvesh Patil <sarvesh2902@gmail.com>
2023-04-04 08:36:52 +0530

    2148. Count Elements With Strictly Smaller and Greater Elements

---
+++ leetcode/2148.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/count-elements-with-strictly-smaller-and-greater-elements/
+
+class Solution
+{
+public:
+  int countElements(vector<int> &nums)
+  {
+    sort(nums.begin(), nums.end());
+    int ans = 0;
+
+    for (int i = 1; i < nums.size() - 1; i++)
+    {
+      if (nums[0] < nums[i] && nums[nums.size() - 1] > nums[i])
+      {
+        ans++;
+      }
+    }
+
+    return ans;
+  }
+};
