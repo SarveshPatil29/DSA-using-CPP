dcbb335f949c4a3b8899a8593db78b937093c431
Sarvesh Patil <sarvesh2902@gmail.com>
2023-04-13 19:31:00 +0530

    optimized 946

--- leetcode/0946.cpp
+++ leetcode/0946.cpp
@@ -5,40 +5,62 @@ class Solution
 public:
   bool validateStackSequences(vector<int> &pushed, vector<int> &popped)
   {
-    vector<int> nums{-1};
-
-    int i = 0;
+    vector<int> stack;
     int j = 0;
 
-    while (i < pushed.size() || j < popped.size())
+    for (int i = 0; i < pushed.size(); i++)
     {
-      if (i < pushed.size() && nums.back() == popped[j])
+      stack.push_back(pushed[i]);
+      while (j < popped.size() && stack.size() && popped[j] == stack[stack.size() - 1])
       {
-        nums.pop_back();
+        stack.pop_back();
         j++;
-      }
-      else if (i < pushed.size() && nums.back() != popped[j])
-      {
-        nums.push_back(pushed[i]);
-        i++;
-      }
-      else
-      {
-        while (nums.size() > 1)
-        {
-          if (nums.back() != popped[j])
-          {
-            return false;
-          }
-          else
-          {
-            nums.pop_back();
-            j++;
-          }
-        }
       }
     }
 
-    return true;
+    return !stack.size();
   }
 };
+
+// class Solution
+// {
+// public:
+//   bool validateStackSequences(vector<int> &pushed, vector<int> &popped)
+//   {
+//     vector<int> nums{-1};
+
+//     int i = 0;
+//     int j = 0;
+
+//     while (i < pushed.size() || j < popped.size())
+//     {
+//       if (i < pushed.size() && nums.back() == popped[j])
+//       {
+//         nums.pop_back();
+//         j++;
+//       }
+//       else if (i < pushed.size() && nums.back() != popped[j])
+//       {
+//         nums.push_back(pushed[i]);
+//         i++;
+//       }
+//       else
+//       {
+//         while (nums.size() > 1)
+//         {
+//           if (nums.back() != popped[j])
+//           {
+//             return false;
+//           }
+//           else
+//           {
+//             nums.pop_back();
+//             j++;
+//           }
+//         }
+//       }
+//     }
+
+//     return true;
+//   }
+// };
