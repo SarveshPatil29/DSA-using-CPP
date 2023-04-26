0801a2254a08affbe2c80809ac76fe51d99cc9dd
Sarvesh Patil <sarvesh2902@gmail.com>
2023-04-26 06:40:09 +0530

    258. Add Digits

---
+++ leetcode/0258.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/add-digits/
+
+class Solution
+{
+public:
+  int addDigits(int num)
+  {
+    while (num > 9)
+    {
+      int sum = 0;
+      while (num)
+      {
+        sum += num % 10;
+        num /= 10;
+      }
+      num = sum;
+    }
+    return num;
+  }
+};
