131448167f59579e5fc08b35ff8f9830c3508bd6
Sarvesh Patil <sarvesh2902@gmail.com>
2023-05-01 11:23:49 +0530

    updated 46 leetcode

--- leetcode/0046.cpp
+++ leetcode/0046.cpp
@@ -26,3 +26,31 @@ private:
     }
   }
 };
+
+// class Solution {
+// public:
+//     void solve(vector<int> &nums, vector<vector<int>> &ans, set<int> &mp, vector<int> &temp, int size) {
+//         if(temp.size() == size) {
+//             ans.push_back(temp);
+//             return;
+//         }
+
+//         for(int i = 0; i<size; i++) {
+//             if(!mp.count(nums[i])) {
+//                 mp.insert(nums[i]);
+//                 temp.push_back(nums[i]);
+//                 solve(nums, ans, mp, temp, size);
+//                 mp.erase(nums[i]);
+//                 temp.pop_back();
+//             }
+//         }
+//     }
+
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         vector<int> temp;
+//         set<int> mp;
+//         solve(nums, ans, mp, temp, nums.size());
+//         return ans;
+//     }
+// };
