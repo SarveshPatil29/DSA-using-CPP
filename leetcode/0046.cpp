37946e97839bd4617938a48826e28c64884e2f3a
Sarvesh Patil <sarvesh2902@gmail.com>
2023-05-01 11:43:43 +0530

    optimized 46

--- leetcode/0046.cpp
+++ leetcode/0046.cpp
@@ -1,31 +1,31 @@
 // https://leetcode.com/problems/permutations/
 
-class Solution
-{
-public:
-  vector<vector<int>> permute(vector<int> &nums)
-  {
-    vector<vector<int>> result;
-    dfs(nums, 0, result);
-    return result;
-  }
+// class Solution
+// {
+// public:
+//   vector<vector<int>> permute(vector<int> &nums)
+//   {
+//     vector<vector<int>> result;
+//     dfs(nums, 0, result);
+//     return result;
+//   }
 
-private:
-  void dfs(vector<int> &nums, int start, vector<vector<int>> &result)
-  {
-    if (start == nums.size())
-    {
-      result.push_back(nums);
-      return;
-    }
-    for (int i = start; i < nums.size(); i++)
-    {
-      swap(nums[i], nums[start]);
-      dfs(nums, start + 1, result);
-      swap(nums[i], nums[start]);
-    }
-  }
-};
+// private:
+//   void dfs(vector<int> &nums, int start, vector<vector<int>> &result)
+//   {
+//     if (start == nums.size())
+//     {
+//       result.push_back(nums);
+//       return;
+//     }
+//     for (int i = start; i < nums.size(); i++)
+//     {
+//       swap(nums[i], nums[start]);
+//       dfs(nums, start + 1, result);
+//       swap(nums[i], nums[start]);
+//     }
+//   }
+// };
 
 // class Solution {
 // public:
@@ -54,3 +54,30 @@ private:
 //         return ans;
 //     }
 // };
+
+class Solution
+{
+public:
+  void solve(vector<vector<int>> &ans, vector<int> &nums, int size, int ind)
+  {
+    if (ind >= size)
+    {
+      ans.push_back(nums);
+      return;
+    }
+
+    for (int i = ind; i < size; i++)
+    {
+      swap(nums[i], nums[ind]);
+      solve(ans, nums, size, ind + 1);
+      swap(nums[i], nums[ind]);
+    }
+  }
+
+  vector<vector<int>> permute(vector<int> &nums)
+  {
+    vector<vector<int>> ans;
+    solve(ans, nums, nums.size(), 0);
+    return ans;
+  }
+};
