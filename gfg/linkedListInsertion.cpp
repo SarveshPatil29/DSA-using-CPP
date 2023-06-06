258405752871d55e2c53963fe9f29289d60ee3e9
Sarvesh Patil <sarvesh2902@gmail.com>
2023-06-06 19:36:44 +0530

    Linked List Insertion

---
+++ gfg/linkedListInsertion.cpp
@@ -0,0 +1,31 @@
+// https://practice.geeksforgeeks.org/problems/linked-list-insertion-1587115620/0
+
+class Solution
+{
+public:
+  // Function to insert a node at the beginning of the linked list.
+  Node *insertAtBegining(Node *head, int x)
+  {
+    Node *newNode = new Node(x);
+    newNode->next = head;
+    head = newNode;
+    return head;
+  }
+
+  // Function to insert a node at the end of the linked list.
+  Node *insertAtEnd(Node *head, int x)
+  {
+    Node *newNode = new Node(x);
+    if (head == NULL)
+      return newNode;
+
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+      temp = temp->next;
+    }
+
+    temp->next = newNode;
+    return head;
+  }
+};
