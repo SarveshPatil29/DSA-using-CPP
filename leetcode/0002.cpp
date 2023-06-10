fcd0db7dba5755942088063c26ea4912794bb845
Sarvesh Patil <sarvesh2902@gmail.com>
2023-06-10 17:06:48 +0530

    2. Add Two Numbers

---
+++ leetcode/0002.cpp
@@ -0,0 +1,87 @@
+// https://leetcode.com/problems/add-two-numbers/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+  {
+    int carry = 0;
+    int sum = l1->val + l2->val;
+
+    if (sum >= 10)
+    {
+      sum -= 10;
+      carry = 1;
+    }
+
+    ListNode *l3 = new ListNode(sum);
+    l1 = l1->next;
+    l2 = l2->next;
+    ListNode *curr = l3;
+
+    while (l1 && l2)
+    {
+      sum = l1->val + l2->val + carry;
+      carry = 0;
+      if (sum >= 10)
+      {
+        sum -= 10;
+        carry = 1;
+      }
+      ListNode *temp = new ListNode(sum);
+      curr->next = temp;
+      curr = curr->next;
+      l1 = l1->next;
+      l2 = l2->next;
+    }
+
+    while (l1)
+    {
+      sum = l1->val + carry;
+      carry = 0;
+      if (sum >= 10)
+      {
+        sum -= 10;
+        carry = 1;
+      }
+      ListNode *temp = new ListNode(sum);
+      curr->next = temp;
+      curr = curr->next;
+      l1 = l1->next;
+    }
+
+    while (l2)
+    {
+      sum = l2->val + carry;
+      carry = 0;
+      if (sum >= 10)
+      {
+        sum -= 10;
+        carry = 1;
+      }
+      ListNode *temp = new ListNode(sum);
+      curr->next = temp;
+      curr = curr->next;
+      l2 = l2->next;
+    }
+
+    if (carry)
+    {
+      ListNode *temp = new ListNode(1);
+      curr->next = temp;
+      curr = curr->next;
+    }
+
+    return l3;
+  }
+};
