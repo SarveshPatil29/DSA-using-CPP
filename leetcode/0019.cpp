0705758cd0321eb5831ccc39c0ad74b86b027f18
Sarvesh Patil <sarvesh2902@gmail.com>
2023-06-10 16:36:28 +0530

    2095. Delete the Middle Node of a Linked List

--- leetcode/0019.cpp
+++ leetcode/0019.cpp
@@ -1,5 +1,55 @@
 // https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution
+// {
+// public:
+//   ListNode *removeNthFromEnd(ListNode *head, int n)
+//   {
+//     if (head == NULL || head->next == NULL)
+//     {
+//       return NULL;
+//     }
+
+//     int length = 0;
+//     ListNode *temp = head;
+
+//     while (temp)
+//     {
+//       length++;
+//       temp = temp->next;
+//     }
+
+//     cout << length << " ";
+
+//     int toRemove = length - n;
+//     temp = head;
+//     cout << toRemove << " ";
+//     if (toRemove < 1)
+//     {
+//       head = head->next;
+//       return head;
+//     }
+//     while (toRemove > 1)
+//     {
+//       temp = temp->next;
+//       toRemove--;
+//     }
+
+//     temp->next = temp->next->next;
+//     return head;
+//   }
+// };
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -15,37 +65,28 @@ class Solution
 public:
   ListNode *removeNthFromEnd(ListNode *head, int n)
   {
-    if (head == NULL || head->next == NULL)
+    ListNode *fast = head;
+    ListNode *slow = head;
+
+    while (n)
     {
-      return NULL;
+      fast = fast->next;
+      n--;
     }
 
-    int length = 0;
-    ListNode *temp = head;
-
-    while (temp)
-    {
-      length++;
-      temp = temp->next;
-    }
-
-    cout << length << " ";
-
-    int toRemove = length - n;
-    temp = head;
-    cout << toRemove << " ";
-    if (toRemove < 1)
+    if (fast == NULL)
     {
       head = head->next;
       return head;
     }
-    while (toRemove > 1)
+
+    while (fast->next)
     {
-      temp = temp->next;
-      toRemove--;
+      fast = fast->next;
+      slow = slow->next;
     }
 
-    temp->next = temp->next->next;
+    slow->next = slow->next->next;
     return head;
   }
 };
