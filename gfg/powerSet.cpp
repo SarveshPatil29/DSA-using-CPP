543c03d90ed957d71faef452229c8f199b36173b
Sarvesh Patil <sarvesh2902@gmail.com>
2023-06-16 16:54:33 +0530

    solved using bit manipulation

--- gfg/powerSet.cpp
+++ gfg/powerSet.cpp
@@ -1,29 +1,53 @@
 // https://practice.geeksforgeeks.org/problems/power-set4302/1
 
+// class Solution
+// {
+// public:
+//   void solve(string s, string &ds, vector<string> &ans, int ind)
+//   {
+//     if (ind == s.size())
+//     {
+//       if (ds.size())
+//         ans.push_back(ds);
+//       return;
+//     }
+
+//     ds.push_back(s[ind]);
+//     solve(s, ds, ans, ind + 1);
+
+//     ds.pop_back();
+//     solve(s, ds, ans, ind + 1);
+//   }
+
+//   vector<string> AllPossibleStrings(string s)
+//   {
+//     vector<string> ans;
+//     string ds = "";
+//     solve(s, ds, ans, 0);
+//     sort(ans.begin(), ans.end());
+//     return ans;
+//   }
+// };
+
 class Solution
 {
 public:
-  void solve(string s, string &ds, vector<string> &ans, int ind)
-  {
-    if (ind == s.size())
-    {
-      if (ds.size())
-        ans.push_back(ds);
-      return;
-    }
-
-    ds.push_back(s[ind]);
-    solve(s, ds, ans, ind + 1);
-
-    ds.pop_back();
-    solve(s, ds, ans, ind + 1);
-  }
-
   vector<string> AllPossibleStrings(string s)
   {
     vector<string> ans;
-    string ds = "";
-    solve(s, ds, ans, 0);
+    int n = s.size();
+    for (int i = 1; i < (1 << n); i++)
+    {
+      string str = "";
+      for (int j = 0; j < n; j++)
+      {
+        if (i & (1 << j))
+        {
+          str += s[j];
+        }
+      }
+      ans.push_back(str);
+    }
     sort(ans.begin(), ans.end());
     return ans;
   }
--- leetcode/0078.cpp
+++ leetcode/0078.cpp
@@ -1,28 +1,55 @@
 // https://leetcode.com/problems/subsets/
 
+// class Solution
+// {
+// public:
+//   vector<vector<int>> subsetsHelper(vector<int> &nums, int n)
+//   {
+//     if (n >= nums.size())
+//     {
+//       return {{}};
+//     }
+//     vector<vector<int>> partialAns = subsetsHelper(nums, n + 1);
+//     vector<vector<int>> ans(partialAns);
+
+//     for (vector<int> item : partialAns)
+//     {
+//       item.push_back(nums[n]);
+//       ans.push_back(item);
+//     }
+
+//     return ans;
+//   }
+
+//   vector<vector<int>> subsets(vector<int> &nums)
+//   {
+//     return subsetsHelper(nums, 0);
+//   }
+// };
+
 class Solution
 {
 public:
-  vector<vector<int>> subsetsHelper(vector<int> &nums, int n)
+  void solve(vector<int> s, vector<int> &ds, vector<vector<int>> &ans, int ind)
   {
-    if (n >= nums.size())
+    if (ind == s.size())
     {
-      return {{}};
-    }
-    vector<vector<int>> partialAns = subsetsHelper(nums, n + 1);
-    vector<vector<int>> ans(partialAns);
-
-    for (vector<int> item : partialAns)
-    {
-      item.push_back(nums[n]);
-      ans.push_back(item);
+      ans.push_back(ds);
+      return;
     }
 
-    return ans;
+    ds.push_back(s[ind]);
+    solve(s, ds, ans, ind + 1);
+
+    ds.pop_back();
+    solve(s, ds, ans, ind + 1);
   }
 
   vector<vector<int>> subsets(vector<int> &nums)
   {
-    return subsetsHelper(nums, 0);
+    vector<vector<int>> ans;
+    vector<int> ds;
+    solve(nums, ds, ans, 0);
+    return ans;
   }
 };
